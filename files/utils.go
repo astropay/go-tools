1297436fb77667a2971b69a8ac3aff3b8e7da7ca
Agustín Pazos <agustin@astropay.com>
2018-01-17 18:12:08 -0300

    Add feature to handle files

--- files/utils.go
+++ files/utils.go
@@ -5,7 +5,15 @@
 package files
 
 import (
+	"errors"
 	"os"
+)
+
+// File errors
+var (
+	ErrGetFileInfo       = errors.New("Error get FileInfo")
+	ErrFileAlreadyExists = errors.New("File already exists")
+	ErrCreateFile        = errors.New("Error create file")
 )
 
 // Exists returns true/false depending if the file indicated in path, exists or not
@@ -14,4 +22,29 @@ func Exists(path string) bool {
 		return true
 	}
 	return false
+}
+
+// CreateFile create a file from path.
+func CreateFile(path string) (err error) {
+	_, err = os.Stat(path)
+
+	if os.IsExist(err) {
+		return ErrFileAlreadyExists
+	}
+
+	var file *os.File
+	file, err = os.Create(path)
+	defer file.Close()
+	if err != nil {
+		return ErrCreateFile
+	}
+
+	return
+}
+
+// DeleteFile delete a file from path
+func DeleteFile(path string) (err error) {
+	// delete file
+	err = os.Remove(path)
+	return
 }
